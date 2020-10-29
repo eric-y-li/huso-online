3b6221d888fdf3ec828b765d54a7fe48f6c158d9
Eric Li <ericli52034@gmail.com>
2020-10-29 18:49:02 -0400

    Add components directory

---
+++ client/src/components/CompetitorForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class CompetitorForm extends React.Component {
+    constructor(props) {
+        super(props);
+        // unused at the moment - feel free to incorporate or modify
+        this.state = {
+            tournamentId: "",
+            userId: ""
+        }
+    }
+
+    render() {
+        return (
+            // Render elements here! Look into material-ui or something similar
+            // for out-of-the-box components (ie. Typography)
+            <div>
+                <h1>Stuff Goes Here</h1>
+            </div>    
+        );
+    }   
+}
